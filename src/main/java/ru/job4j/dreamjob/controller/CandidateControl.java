a592c69db800bfc01f844e80d5dcbd317b3ad93e
Grossevich Lev <levgross@gmail.com>
2022-09-16 12:08:39 +0400

    8. HTML form. Создание кандидата.

--- src/main/java/ru/job4j/dreamjob/controller/CandidateControl.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateControl.java
@@ -3,7 +3,11 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.dreamjob.model.Candidate;
+import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.store.CandidateStore;
+
+import java.time.LocalDate;
 
 @Controller
 public class CandidateControl {
@@ -13,5 +17,13 @@ public class CandidateControl {
     public String candidates(Model model) {
         model.addAttribute("candidates", store.findAll());
         return "candidates";
+    }
+
+    @GetMapping("/formAddCandidate")
+    public String addPost(Model model) {
+        model.addAttribute("candidate", new Candidate(
+                0, "Заполните имя", "Заполните описание", LocalDate.now()
+        ));
+        return "addCandidate";
     }
 }
