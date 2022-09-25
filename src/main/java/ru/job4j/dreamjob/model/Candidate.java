73d38b906c01d26799272566b15b29f378ab4b44
Grossevich Lev <levgross@gmail.com>
2022-09-25 18:35:43 +0400

    4. Формы. Загрузка файла на сервер

--- src/main/java/ru/job4j/dreamjob/model/Candidate.java
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -8,6 +8,7 @@ public class Candidate {
     private String name;
     private String desc;
     private LocalDateTime created;
+    private byte[] photo;
 
     public Candidate(int id, String name, String desc, LocalDateTime created) {
         this.id = id;
@@ -46,6 +47,14 @@ public class Candidate {
 
     public void setCreated(LocalDateTime created) {
         this.created = created;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
     }
 
     @Override
