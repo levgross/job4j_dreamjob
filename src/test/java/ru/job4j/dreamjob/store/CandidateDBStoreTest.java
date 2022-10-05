74aa623d0625ad48ac238270db7b84552f879ec3
Grossevich Lev <levgross@gmail.com>
2022-10-05 19:00:56 +0400

    3. Тестирование базы данных. Liquibase H2

--- src/test/java/ru/job4j/dreamjob/store/CandidateDBStoreTest.java
+++ src/test/java/ru/job4j/dreamjob/store/CandidateDBStoreTest.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.*;
 
 class CandidateDBStoreTest {
     @Test
-    public void whenCreateCandidate() {
+    public void whenAddCandidate() {
         CandidateDBStore store = new CandidateDBStore(new Main().loadPool());
         Candidate candidate = new Candidate(0, "John Smith", "Description", LocalDateTime.now());
         candidate.setPhoto(new byte[1028]);
@@ -20,5 +20,32 @@ class CandidateDBStoreTest {
         assertThat(candidateInDB.getDesc()).isEqualTo(candidate.getDesc());
         assertThat(candidateInDB.getCreated()).isEqualTo(candidate.getCreated());
         assertThat(candidateInDB.getPhoto()).isEqualTo(candidate.getPhoto());
+    }
+
+    @Test
+    public void whenUpdateCandidate() {
+        CandidateDBStore store = new CandidateDBStore(new Main().loadPool());
+        Candidate candidate1 = new Candidate(0, "John Smith", "Description", LocalDateTime.now());
+        candidate1.setPhoto(new byte[1028]);
+        store.add(candidate1);
+        Candidate candidate2 = new Candidate(0, "Sam Johnson", "Description2", LocalDateTime.now());
+        candidate2.setPhoto(new byte[514]);
+        Candidate candidateInDB = store.findById(0);
+        assertThat(candidateInDB.getName()).isEqualTo(candidate2.getName());
+        assertThat(candidateInDB.getDesc()).isEqualTo(candidate2.getDesc());
+        assertThat(candidateInDB.getCreated()).isEqualTo(candidate2.getCreated());
+        assertThat(candidateInDB.getPhoto()).isEqualTo(candidate2.getPhoto());
+    }
+
+    @Test
+    public void whenFindAllCandidates() {
+        CandidateDBStore store = new CandidateDBStore(new Main().loadPool());
+        Candidate candidate1 = new Candidate(0, "John Smith", "Description", LocalDateTime.now());
+        candidate1.setPhoto(new byte[1028]);
+        store.add(candidate1);
+        Candidate candidate2 = new Candidate(1, "Sam Johnson", "Description2", LocalDateTime.now());
+        candidate2.setPhoto(new byte[514]);
+        store.add(candidate2);
+        assertThat(store.findAll()).containsExactly(candidate1, candidate2);
     }
 }
--- src/test/java/ru/job4j/dreamjob/store/PostDBStoreTest.java
+++ src/test/java/ru/job4j/dreamjob/store/PostDBStoreTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.*;
 
 class PostDBStoreTest {
     @Test
-    public void whenCreatePost() {
+    public void whenAddPost() {
         PostDBStore store = new PostDBStore(new Main().loadPool());
         Post post = new Post(0, "Java Job", "Description", LocalDateTime.now());
         post.setCity(new City(0, "NYC"));
@@ -23,5 +23,38 @@ class PostDBStoreTest {
         assertThat(postInDB.getCreated()).isEqualTo(post.getCreated());
         assertThat(postInDB.getCity()).isEqualTo(post.getCity());
         assertThat(postInDB.isVisible()).isEqualTo(post.isVisible());
+    }
+
+    @Test
+    public void whenFindAllPosts() {
+        PostDBStore store = new PostDBStore(new Main().loadPool());
+        Post post1 = new Post(0, "Java Job", "Description", LocalDateTime.now());
+        post1.setCity(new City(0, "NYC"));
+        post1.setVisible(true);
+        store.add(post1);
+        Post post2 = new Post(1, "Java Job Dev", "Description Dev", LocalDateTime.now());
+        post2.setCity(new City(1, "Moscow"));
+        post2.setVisible(false);
+        store.add(post2);
+        assertThat(store.findAll()).containsExactly(post1, post2);
+    }
+
+    @Test
+    public void whenUpdatePost() {
+        PostDBStore store = new PostDBStore(new Main().loadPool());
+        Post post1 = new Post(0, "Java Job", "Description", LocalDateTime.now());
+        post1.setCity(new City(0, "NYC"));
+        post1.setVisible(true);
+        store.add(post1);
+        Post post2 = new Post(0, "Java Job Dev", "Description Dev", LocalDateTime.now());
+        post2.setCity(new City(1, "Moscow"));
+        post2.setVisible(false);
+        store.update(post2);
+        Post postInDB = store.findById(0);
+        assertThat(postInDB.getName()).isEqualTo(post2.getName());
+        assertThat(postInDB.getDescription()).isEqualTo(post2.getDescription());
+        assertThat(postInDB.getCreated()).isEqualTo(post2.getCreated());
+        assertThat(postInDB.getCity()).isEqualTo(post2.getCity());
+        assertThat(postInDB.isVisible()).isEqualTo(post2.isVisible());
     }
 }
